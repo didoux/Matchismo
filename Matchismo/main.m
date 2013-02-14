8bf3b506f7d0c4ec26fdb3da683b510438ce9073
Dan Idoux <didoux@Dans-MacBook-Pro.local>
2013-02-13 22:00:31 -0600

    Initial Commit

---
+++ Matchismo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Dan Idoux on 2/13/13.
+//  Copyright (c) 2013 Dan Idoux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+    }
+}
