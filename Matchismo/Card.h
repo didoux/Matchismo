6e4be02fd679f5e24a3627ad0130abdb14a54baa
Dan Idoux <didoux@charter.net>
2013-02-18 19:53:44 -0600

    Initial Comment

---
+++ Matchismo/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by Dan Idoux on 2/17/13.
+//  Copyright (c) 2013 Dan Idoux. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ Matchismo/Card.m
@@ -0,0 +1,13 @@
+ //
+//  Card.m
+//  Matchismo
+//
+//  Created by Dan Idoux on 2/17/13.
+//  Copyright (c) 2013 Dan Idoux. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
